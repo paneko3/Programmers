ce193d94a92010055db045c5e962a381924b9947
JaeCheol <34058812+paneko3@users.noreply.github.com>
2023-03-11 23:48:39 +0900

    Programmers_17687_2018 KAKAO BLIND RECRUITMENT_[3차] n진수 게임

---
+++ 2018 KAKAO BLIND RECRUITMENT/Programmers_17687.java
@@ -0,0 +1,21 @@
+// Programmers_17687_2018 KAKAO BLIND RECRUITMENT_[3차] n진수 게임
+
+import java.util.Arrays;
+
+public class Programmers_17687 {
+    public static void main(String[] args){
+        int n = 2;
+        int t = 4;
+        int m = 2;
+        int p = 1;
+        Solution sol = new Solution();
+        System.out.println(sol.solution(n,t,m,p));
+    }
+    static class Solution {
+        public String solution(int n, int t, int m, int p) {
+            String answer = "";
+            return answer;
+        }
+    }
+}
+
